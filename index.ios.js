06dd052019ff9cbca4ba6c9b2e1b79b3d90bc21d
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2017-01-02 15:02:33 -0800

    Working loading page

--- index.ios.js
+++ index.ios.js
@@ -9,27 +9,68 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  NavigatorIOS
 } from 'react-native';
 
-export default class PetSwipe extends Component {
+// class Navigator extends Component {
+//   render() {
+//     return (
+//       <NavigatorIOS
+//         initialRoute={{
+//           component: Welcome,
+//           title: 'Welcome',
+//         }}
+//       style={{flex: 1}}
+//       />
+//     )
+//   }
+// }
+
+class PetSwipe extends Component {
   render() {
+    let welcomeImage = {
+      uri: 'https://thumbs.dreamstime.com/z/cartoon-cute-pets-animals-set-illustration-little-baby-35925102.jpg'
+    };
+
     return (
+      // <View style={{flex: 1, backgroundColor: '#8AA2A9'}}>
+      //   <Text style={styles.container}>
+      //     Hello?
+      //   </Text>
+      // </View>
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Pet Swipe!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          This is going to be fun...
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={welcomeImage} style={{width: 240, height: 240}}/>
       </View>
     );
   }
 }
+
+// class Welcome extends Component {
+//   render() {
+//
+//     return (
+//       // Try removing the `flex: 1` on the parent View.
+//       // The parent will not have dimensions, so the children can't expand.
+//       // What if you add `height: 300` instead of `flex: 1`?
+//       <View style={{flex: 1}}>
+//         <View style={styles.container}>
+//           <Image source={welcomeImage} style={{width: 240, height: 240}}/>
+//           // <Text style={styles.whatIsIt}>
+//           //   Pet Swipe!
+//           // </Text>
+//         </View>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -47,7 +88,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('PetSwipe', () => PetSwipe);
