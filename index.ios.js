73713c5bb47a6f9e3c925559edaeadbbd2f518d0
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2017-01-02 13:37:47 -0800

    Testing git communication

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class PetSwipe extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Pet Swipe!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
