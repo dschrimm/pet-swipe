b61919ad9966356905c7e5fdab3195ba7680385d
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2017-01-03 13:55:22 -0800

    Initial ListView components

--- index.ios.js
+++ index.ios.js
@@ -4,30 +4,37 @@
  * @flow
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   Image,
-  NavigatorIOS,
-  TouchableHighlight
+  ListView,
 } from 'react-native';
 
-// class Navigator extends Component {
-//   render() {
-//     return (
-//       <NavigatorIOS
-//         initialRoute={{
-//           component: PetSwipe,
-//           title: 'Welcome',
-//         }}
-//       style={{flex: 1}}
-//       />
-//     )
-//   }
-// }
+
+class Welcome extends Component {
+
+  render() {
+    let welcomeImage = {
+      uri: 'https://thumbs.dreamstime.com/z/cartoon-cute-pets-animals-set-illustration-little-baby-35925102.jpg'
+    };
+    return (
+      // Welcome/loading page
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcome}>
+          Welcome to Pet Swipe!
+        </Text>
+        <Text style={styles.instructions}>
+          This is going to be fun...
+        </Text>
+        <Image source={welcomeImage} style={{width: 240, height: 240}}/>
+      </View>
+    )
+  }
+}
 
 class PetSwipe extends Component {
   // static propTypes = {
@@ -47,9 +54,6 @@ class PetSwipe extends Component {
   // }
 
   render() {
-    let welcomeImage = {
-      uri: 'https://thumbs.dreamstime.com/z/cartoon-cute-pets-animals-set-illustration-little-baby-35925102.jpg'
-    };
 
     let petImage = {
       uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/37055772/1/?bust=1482790177&width=632&no_scale_up=1'
@@ -70,22 +74,13 @@ class PetSwipe extends Component {
       //   </TouchableHighlight>
       // </View>
 
-      // // Welcome/loading page
-      // <View style={styles.container}>
-      //   <Text style={styles.welcome}>
-      //     Welcome to Pet Swipe!
-      //   </Text>
-      //   <Text style={styles.instructions}>
-      //     This is going to be fun...
-      //   </Text>
-      //   <Image source={welcomeImage} style={{width: 240, height: 240}}/>
-      // </View>
-
+      // <Text>Second screen</Text>
       // Browse pets
+      // https://www.petfinder.com/petdetail/37055772
       <View style={styles.container}>
         <Image source={petImage} style={{flex: 2}}>
         <View style={styles.backdrop}>
-        <Text style={styles.briefDescription}>Pet name, age, breed</Text>
+        <Text style={styles.briefDescription}>Annika, German Shepherd</Text>
         </View>
         </Image>
       </View>
@@ -112,23 +107,95 @@ class PetSwipe extends Component {
 //   }
 // }
 
+// Favorites page
+class Favorites extends Component {
+  // render() {
+    // let imageOne = {
+    //   uri: 'https://thumbs.dreamstime.com/z/cartoon-cute-pets-animals-set-illustration-little-baby-35925102.jpg'
+    // };
+    // let imageTwo = {
+    //   uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/36618931/3/?bust=1478714805&width=632&no_scale_up=1'
+  //   };
+  //   return (
+  //     <View style={styles.container}>
+  //       <Text style={styles.welcome}>
+  //         Welcome to Pet Swipe!
+  //       </Text>
+  //       <Text style={styles.instructions}>
+  //         This is going to be fun...
+  //       </Text>
+  //       <Image source={imageOne} style={{width: 240, height: 240}}/>
+  //     </View>
+  //   )
+  // }
+  constructor() {
+    let imageOne = {
+      uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/37055772/1/?bust=1482790177&width=632&no_scale_up=1'
+    };
+    let imageTwo = {
+      uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/36618931/3/?bust=1478714805&width=632&no_scale_up=1'
+    };
+    let imageThree = {
+      uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/36687740/1/?bust=1478648999&width=632&no_scale_up=1'
+    };
+    let imageFour = {
+      uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/37064665/1/?bust=1482900212&width=632&no_scale_up=1'
+    };
+    let imageFive = {
+      uri: 'https://drpem3xzef3kf.cloudfront.net/photos/pets/36490552/1/?bust=1480459899&width=632&no_scale_up=1'
+    };
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([imageOne, imageTwo, imageThree, imageFour, imageFive]),
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Image source={rowData} style={{width: 140, height: 140}}/>}
+      />
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+  },
+  welcomeContainer: {
     flex: 1,
-    justifyContent: 'space-between',
-    // alignContent: 'space-between',
-    // flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'black'
+    justifyContent: 'center',
+    backgroundColor: '#90BAAD'
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+    color: '#717C89',
+    fontWeight: 'bold'
+  },
+  container: {
+    paddingTop: 25,
+    backgroundColor: '#90BAAD',
+    flex: 1,
+    // justifyContent: 'space-between',
+    // alignContent: 'space-between',
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // backgroundColor: 'rgba(0,0,0,0)',
+    // width: Dimensions.get('window').width,
+    // height: Dimensions.get('window').height
+    // resizeMode: 'cover'
   },
   instructions: {
+    fontSize: 20,
     textAlign: 'center',
-    color: '#333333',
+    color: '#717C89',
     marginBottom: 5,
   },
   backdrop: {
@@ -141,10 +208,10 @@ const styles = StyleSheet.create({
   },
   briefDescription: {
     fontSize: 20,
-    textAlign: 'center',
+    // textAlign: 'center',
     backgroundColor: 'rgba(0,0,0,0)',
     color: 'white'
   }
 });
 
-AppRegistry.registerComponent('PetSwipe', () => PetSwipe);
+AppRegistry.registerComponent('PetSwipe', () => Favorites);
