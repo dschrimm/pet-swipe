35817b28af91baf0ebcbca0e4b37d0908073ea5c
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2017-01-09 17:26:59 -0800

    Created app.js file that successfully makes a get request to a hard coded Petfinder API path

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.get('/', function(req, res) {
+  request.get({ url: "http://api.petfinder.com/pet.find?format=json&key=f2e828ff92a5d99a6e9ff79e10ca558a&animal=dog&location=98144&size=L&callback=?" }, function(error, response, body) {
+    console.log(response);
+    res.send(response);
+  });
+  // res.sendFile(__dirname + '/index.html');
+  // res.send(res.body)
+  // console.log(res);
+});
+
+app.listen(3000, function() {
+  console.log('listening on 3000');
+});
